e2dbcb30be8aee0cca6c052c3c80443f2d719d60
yanhui wang <yanhuiwang@yanhuidemacbook-pro.local>
2016-08-21 22:51:35 +0800

    Initial Commit

---
+++ Choice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Choice
+//
+//  Created by yanhui wang on 16/8/21.
+//  Copyright © 2016年 yanhui wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Choice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Choice
+//
+//  Created by yanhui wang on 16/8/21.
+//  Copyright © 2016年 yanhui wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
